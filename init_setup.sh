e45c4d4487f6f5aea43fad1d8cd0c6d2b1fe522e
Syed Sajjad Askari <syedsajjad62@gmail.com>
2023-10-02 05:00:03 +0000

    creatig env and install dependences

---
+++ init_setup.sh
@@ -0,0 +1,10 @@
+echo [$(date)]: "CRETING PYTHON ENVIRNEMNT"
+echo [$(date)]: "START"
+echo [$(date)]: "creating env with python 3.10 version" 
+conda create --prefix ./env python=3.10 -y
+echo [$(date)]: "activating the environment" 
+source activate ./env
+echo [$(date)]: "ENVIRNMENT successfully created"
+echo [$(date)]: "INSTALLING requiremnts,txt"
+pip install -r requirements.txt
+echo [$(date)]: "END"
